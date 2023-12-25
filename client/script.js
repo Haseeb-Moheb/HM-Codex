823df9ac45f1c287f8a36a50a865e97ac55eb442
Haseebm01 <haseebdr01@gmail.com>
2023-12-24 23:08:40 -0800

    modified

---
+++ client/script.js
@@ -0,0 +1,7 @@
+import bot from './assets/blog.svg';
+import user from './assets/user.svg';
+
+const form = decument.querySelector('form');
+const chatContainer = document.querySelector('#chat_container');
+
+let loadInterval;
